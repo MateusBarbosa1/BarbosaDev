b840656d18fcf83414847f92c5fd643e65c65903
Mateus Barbosa <pg9779628@gmail.com>
2024-02-27 22:00:37 -0300

    update mailgun

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const DOMAIN = "sandbox7a25e35c2feb42b19b8a73410c13ebdc.mailgun.org";
 const API_KEY = "5d77d82e2b805ea85ab1e48088a688fe";
 
-const PASSWORD = "d968f7b3b77432c3801523a6605b0a75-b7b36bc2-0b8f1a72";
+const PASSWORD = "118d479122bf159476a3f1044003db76-b7b36bc2-a631485a";
 
 const nodemailer = require("nodemailer");
 
